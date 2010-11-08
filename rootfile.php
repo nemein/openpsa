76fcc24029b87b74bc05a95efa8f74fc0ff7a569
Henri Bergius <henri.bergius@iki.fi>
2010-11-08 11:37:33 +0200

    Web app policy paths

--- rootfile.php
+++ rootfile.php
@@ -19,7 +19,8 @@ ini_set('memory_limit', '68M');
 define('MIDCOM_ROOT', realpath(dirname(__FILE__)) . '/lib');
 define('MIDCOM_STATIC_URL', '/openpsa2-static');
 define('OPENPSA2_PREFIX', dirname($_SERVER['SCRIPT_NAME']));
-define('OPENPSA2_THEME_ROOT', MIDCOM_ROOT . '/../themes/');
+// On Debian we load themes from /etc as per web app policy
+define('OPENPSA2_THEME_ROOT', '/etc/openpsa2/themes');
 
 // Include Midgard1 compatibility APIs needed for running OpenPSA under Midgard2
 require(MIDCOM_ROOT . '/ragnaroek-compat.php');
